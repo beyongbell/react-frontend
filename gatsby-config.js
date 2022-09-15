6d6546c9d33453130cc540b622c6846c226e2c32
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2022-09-15 21:18:53 +0700

    First Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,22 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const strapiConfig = {
+  apiURL: process.env.STRAPI_API_URL,
+  accessToken: process.env.STRAPI_TOKEN,
+  collectionTypes: ['product'],
+  singleTypes: [],
+};
+
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-strapi`,
+      options: strapiConfig,
+    },
+  ],
 }
