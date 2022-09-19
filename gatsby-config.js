38d75c6b726f8fbf92546d25a5e59c47d41dd7a6
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2022-09-20 06:58:46 +0700

    Update Promotional Product

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,5 +61,8 @@ module.exports = {
         },
       },
     },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
   ],
 }
--- src/components/home/PromotionalProducts.js
+++ src/components/home/PromotionalProducts.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import Carousel from "react-spring-3d-carousel";
+
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+
+import Carousel from 'react-spring-3d-carousel';
+
 import clsx from "clsx";
+
 import { useStaticQuery, graphql } from "gatsby";
+
 import { makeStyles } from "tss-react/mui";
+
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+import { Link } from "gatsby"
 
 import promoAdornment from "@images/promo-adornment.svg";
 import explore from "@images/explore.svg";
@@ -52,7 +62,7 @@ const useStyles = makeStyles()((theme) => {
       },
     },
     carouselContainer: {
-      marginLeft: "20rem",
+      marginLeft: "0rem",
       [theme.breakpoints.down("md")]: {
         marginLeft: 0,
         height: "30rem",
@@ -82,6 +92,8 @@ const useStyles = makeStyles()((theme) => {
 
 const PromotionalProducts = () => {
   const { classes } = useStyles();
+  const matchesMD = useMediaQuery(theme => theme.breakpoints.down("md"))
+  const [selectedSlide, setSelectedSlide] = useState(0)
   const data = useStaticQuery(graphql`
     query {
       allStrapiProduct(filter: { promotion: { eq: true } }) {
@@ -89,24 +101,75 @@ const PromotionalProducts = () => {
           name
           strapi_id
           description
+          category {
+            name
+          }
           variants {
             images {
-              url
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(width: 200)
+                }
+              }
             }
           }
         }
       }
     }
   `);
-  console.log(data.allStrapiProduct.nodes);
+
+  let slides = []
+  data.allStrapiProduct.nodes.forEach((item, i) => {
+    const image = getImage(item.variants[0].images[0].localFile)
+    slides.push({ 
+      key: i,
+      content: (
+        <Grid container direction="column" alignItems="center">
+          <Grid item>
+            <IconButton
+              disableRipple
+              onClick={() => setSelectedSlide(i)}
+              classes={{
+                root: clsx(classes.iconButton, {
+                  [classes.space]: selectedSlide !== i,
+                }),
+              }}
+            >
+            <GatsbyImage image={image} alt={`image-${i}`} className={classes.carouselImage} objectFit="contain" />
+            </IconButton>
+          </Grid>
+          <Grid item>
+            {selectedSlide === i ? (
+              <Typography variant="h1" classes={{ root: classes.productName }}>
+                {item.category.name.toLowerCase()}
+              </Typography>
+            ) : null}
+          </Grid>
+        </Grid>
+      ),
+      description: item.description,
+      url: `/${item.category.name.toLowerCase()}`,
+    })
+  });
+
   return (
-    <Grid container alignItems="center" classes={{ root: classes.mainContainer }}>
-        <Grid item>
-            Carousel
-        </Grid>
-        <Grid item>
-            Description    
-        </Grid>
+    <Grid container classes={{ root: classes.mainContainer }}  justify={matchesMD ? "space-around" : "space-between"}>
+      <Grid item md={6} classes={{ root: classes.carouselContainer }}>
+        {typeof window !== "undefined" ? (
+          <Carousel slides={slides} goToSlide={selectedSlide} />
+        ) : null}
+      </Grid>
+      <Grid item md={6} classes={{ root: classes.descriptionContainer }} >
+        <Typography variant="h2" paragraph>
+          {slides[selectedSlide].description}
+        </Typography>
+        <Button component={Link} to={slides[selectedSlide].url}>
+          <Typography variant="h4" classes={{ root: classes.explore }}>
+            Explore
+          </Typography>
+          <img src={explore} alt="go to product page" />
+        </Button>
+      </Grid>
     </Grid>
   );
 };
